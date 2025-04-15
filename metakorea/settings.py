70a2d5cf330cd244f3dbfb8675d39e96cba2a41b
signal <metacode10@gmail.com>
2025-04-15 16:08:53 +0900

    a

--- metakorea/settings.py
+++ metakorea/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'metakorea.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbmetacode10',
+        'USER': 'metacode10',
+        'PASSWORD': 'white9407!',
+        'HOST': '211.47.75.102',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
--- userapp/models.py
+++ userapp/models.py
@@ -1,3 +1,28 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+class ZUser(models.Model):
+    zid = models.AutoField(primary_key=True)
+    id = models.CharField(max_length=150, unique=True)
+    name = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)
+    email = models.EmailField(unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    inuse = models.BooleanField(default=True)
+    remark = models.CharField(max_length=300, default=' ')
+    zcme = models.IntegerField()
+    zctm = models.DateTimeField(auto_now_add=True)
+    zcby = models.CharField(max_length=100)
+    zcat = models.CharField(max_length=300)
+    zcre = models.CharField(max_length=100)
+    zume = models.IntegerField()
+    zutm = models.DateTimeField(auto_now_add=True)
+    zuby = models.CharField(max_length=100)
+    zuat = models.CharField(max_length=300)
+    zure = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        db_table = 'zuser'
