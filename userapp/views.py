07ea7152cbd534c374be6c8d7c05158117e3acf8
signal <metacode10@gmail.com>
2025-04-16 11:17:22 +0900

    a

--- userapp/views.py
+++ userapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from userapp.models import ZUser
 
+from django.contrib.auth.hashers import make_password, check_password
+
 
 # Create your views here.
 
@@ -15,10 +17,25 @@ def createUserPost(request):
     user = ZUser()
     user.id = request.POST.get('email', None)
     user.name = request.POST.get('name', None)
-    user.password = request.POST.get('password', None)
+
+    password = request.POST.get('password', None)
+    hashed_password = make_password(password)
+
+    user.password = hashed_password;
     user.inuse = True
     user.email = request.POST.get('email', None)
     user.save()
+
+    # from django.contrib.auth.hashers import make_password, check_password
+    #
+    # # 비밀번호 해싱 – 데이터베이스에 저장할 때
+    # raw_password = 'password123'
+    # hashed_password = make_password(raw_password)
+    # print(f"Hashed Password: {hashed_password}")
+    #
+    # # 비밀번호 검증 – 로그인 시 입력된 비밀번호가 올바른지 확인할 때
+    # is_valid = check_password('password123', hashed_password)
+    # print(f"Password is valid? {is_valid}")
 
     return render(request, "userapp/result.html")
 
